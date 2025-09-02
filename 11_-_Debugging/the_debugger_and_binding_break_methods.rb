a88b1ebd79ade549c4a5c5f2059f6bacd01fa57c
Rodrigo Farah <rodrigo.farah@ferreri.co>
2025-09-02 10:32:55 -0300

    finished chapter 11

---
+++ 11_-_Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,14 @@
+require "debug"
+
+food1 = "Rice"
+food2 = "Beams"
+
+puts "I love to eat #{food1} and #{food2}"
+debugger
+
+color = "Red"
+
+puts "I love color #{color}"
+debugger
+
+puts "End of the program"
